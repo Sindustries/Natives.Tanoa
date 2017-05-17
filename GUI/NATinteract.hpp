3d39ad2f9811e6f56d4756a570cbc83499c61375
Jay <jleach92@hotmail.co.uk>
2017-05-17 12:35:06 +0100

    NEW - INITIAL

---
+++ GUI/NATinteract.hpp
@@ -0,0 +1,136 @@
+class NAT_interact
+{
+	idd = 1650;
+	movingenable = false;
+	enableSimulation = 1;
+
+class controls
+{
+////////////////////////////////////////////////////////
+// GUI EDITOR OUTPUT START (by Jay, v1.063, #Rigube)
+////////////////////////////////////////////////////////
+
+class NATintBtn1: RscButton
+{
+	idc = 1600;
+	text = "INTERACTION 1"; //--- ToDo: Localize;
+	x = 0.438125 * safezoneW + safezoneX;
+	y = 0.291 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn2: RscButton
+{
+	idc = 1601;
+	text = "INTERACTION 2"; //--- ToDo: Localize;
+	x = 0.438125 * safezoneW + safezoneX;
+	y = 0.335 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn3: RscButton
+{
+	idc = 1602;
+	text = "INTERACTION 3"; //--- ToDo: Localize;
+	x = 0.438125 * safezoneW + safezoneX;
+	y = 0.467 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn4: RscButton
+{
+	idc = 1603;
+	text = "INTERACTION 4"; //--- ToDo: Localize;
+	x = 0.438125 * safezoneW + safezoneX;
+	y = 0.511 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn5: RscButton
+{
+	idc = 1604;
+	text = "INTERACTION 5"; //--- ToDo: Localize;
+	x = 0.438125 * safezoneW + safezoneX;
+	y = 0.555 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn6: RscButton
+{
+	idc = 1605;
+	text = "INTERACTION 6"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.291 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn7: RscButton
+{
+	idc = 1606;
+	text = "INTERACTION 7"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.335 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn8: RscButton
+{
+	idc = 1607;
+	text = "INTERACTION 8"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.379 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn9: RscButton
+{
+	idc = 1608;
+	text = "INTERACTION 9"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.423 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn10: RscButton
+{
+	idc = 1609;
+	text = "INTERACTION 10"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.467 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn11: RscButton
+{
+	idc = 1610;
+	text = "INTERACTION 11"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.511 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintBtn12: RscButton
+{
+	idc = 1611;
+	text = "INTERACTION 12"; //--- ToDo: Localize;
+	x = 0.510312 * safezoneW + safezoneX;
+	y = 0.555 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.033 * safezoneH;
+};
+class NATintWheelBX: RscListbox
+{
+	idc = 1500;
+	text = "FRONT RIGHT"; //--- ToDo: Localize;
+	x = 0.438125 * safezoneW + safezoneX;
+	y = 0.379 * safezoneH + safezoneY;
+	w = 0.0670312 * safezoneW;
+	h = 0.077 * safezoneH;
+};
+////////////////////////////////////////////////////////
+// GUI EDITOR OUTPUT END
+////////////////////////////////////////////////////////
+
+
+};
+};
